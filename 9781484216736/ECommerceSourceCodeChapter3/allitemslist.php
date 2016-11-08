319c73ff69d2140ce0cba3fe008a56958d340f4a
NathanPeterson <nalexpeterson@gmail.com>
2016-11-07 22:02:39 -0800

    Initial Commit
    Remove allitemslist.php from zip.

---
+++ 9781484216736/ECommerceSourceCodeChapter3/allitemslist.php
@@ -0,0 +1,34 @@
+<html>
+<head>
+</head>
+<body>
+<?php
+$connect = mysqli_connect("localhost", "root", "gold", "shopping") or die("Please, check your server connection.");
+$query = "SELECT item_code, item_name, description, imagename, price FROM products";
+$results = mysqli_query($connect, $query) or die(mysql_error());
+echo "<table border=\"0\">";
+$x=1;
+echo "<tr>";
+while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))  {
+	if ($x <= 3)
+	{
+		$x = $x + 1;
+ 		extract($row);
+   		echo "<td style=\"padding-right:15px;\">";
+		echo "<a href=itemdetails.php?itemcode=$item_code>";
+		echo '<img src=' . $imagename . ' style="max-width:220px;max-height:240px;width:auto;height:auto;"></img><br/>';
+ 		echo $item_name .'<br/>';
+		echo "</a>";
+    		echo '$'.$price .'<br/>';
+    		echo "</td>";
+	}
+	else
+	{
+		$x=1;
+		echo "</tr><tr>";
+	}
+}
+echo "</table>";
+?>
+</body>
+</html>
