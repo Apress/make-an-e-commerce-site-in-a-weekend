3640d50ea8cdf2748aeee04412f48364d0b05c42
NathanPeterson <nalexpeterson@gmail.com>
2016-11-07 22:12:23 -0800

    Fix bug - Displaying only 14/17 items
    -Bug skipped every fourth item in array.
    -Code now displays all 17 items in array.

--- 9781484216736/ECommerceSourceCodeChapter3/allitemslist.php
+++ 9781484216736/ECommerceSourceCodeChapter3/allitemslist.php
@@ -10,23 +10,21 @@ echo "<table border=\"0\">";
 $x=1;
 echo "<tr>";
 while ($row = mysqli_fetch_array($results, MYSQLI_ASSOC))  {
-	if ($x <= 3)
-	{
-		$x = $x + 1;
- 		extract($row);
-   		echo "<td style=\"padding-right:15px;\">";
-		echo "<a href=itemdetails.php?itemcode=$item_code>";
-		echo '<img src=' . $imagename . ' style="max-width:220px;max-height:240px;width:auto;height:auto;"></img><br/>';
- 		echo $item_name .'<br/>';
-		echo "</a>";
-    		echo '$'.$price .'<br/>';
-    		echo "</td>";
-	}
-	else
-	{
+	if ($x > 3){
 		$x=1;
 		echo "</tr><tr>";
 	}
+
+	extract($row);
+	echo "<td style=\"padding-right:15px;\">";
+	echo "<a href=itemdetails.php?itemcode=$item_code>";
+	echo '<img src=' . $imagename . ' style="max-width:220px;max-height:240px;width:auto;height:auto;"></img><br/>';
+	echo $item_name .'<br/>';
+	echo "</a>";
+	echo '$'.$price .'<br/>';
+	echo "</td>";
+
+	$x = $x + 1;
 }
 echo "</table>";
 ?>
